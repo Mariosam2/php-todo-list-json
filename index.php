2b18769db481729bbba34a8af6e32ece9cd4a9c1
Mariosam2 <marco.mariosaa@gmail.com>
2022-11-30 15:26:05 +0100

    added addTask functionality

--- index.php
+++ index.php
@@ -9,18 +9,30 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.0/axios.min.js" integrity="sha512-OdkysyYNjK4CZHgB+dkw9xQp66hZ9TLqmS2vXaBrftfyJeduVhyy1cOfoxiKdi4/bfgpco6REu6Rb+V2oVIRWg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="./app.js" type="module" defer></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
     <div id="app">
         <main id="site_main">
-            <div class="container">
-                <div class="row">
-                    <ul class="col-12 col-md-7 list-unstyled list-group list-group-flush">
-                        <li v-for="task in tasks" class="list-group-item">{{task.title}}</li>
-                    </ul>
+            <div class="container pt-5">
+                <h1 class="text-center">TODO List</h1>
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-5">
+                        <ul class="tasks list-unstyled list-group list-group-flush my-3">
+                            <li v-for="task in tasks" class="list-group-item fs-4">{{task.title}}</li>
+                        </ul>
+                        <form action="server.php" class="d-flex" method="POST">
+                            <div class="mb-2 flex-grow-1">
+                                <label for="" class="form-label fw-semibold">Add new Task</label>
+                                <input type="text" name="newTask" id="newTask" class="form-control" placeholder="exampleTask" aria-describedby="helpId">
+                                <small id="helpId" class="text-muted">*click on a task to complete it</small>
+                            </div>
+                            <button type="submit" class="btn ms_btn btn-primary flex-grow-1 ms-4" @click="getTasks">Add</button>
+                        </form>
+                    </div>
                 </div>
+
             </div>
         </main>
     </div>
--- server.php
+++ server.php
@@ -1,6 +1,15 @@
 <?php
 $task_string = file_get_contents('tasks.json');
 $tasks = json_decode($task_string, true);
+if (isset($_POST['newTask'])) {
+    array_push($tasks, [
+        'title' => $_POST['newTask'],
+        'completed' => false
+    ]);
+    $task_string = json_encode($tasks);
+    file_put_contents('tasks.json', $task_string);
+    header('Location: index.php');
+}
 //var_dump($tasks);
 header('Content-Type: application/json');
 echo $task_string;
